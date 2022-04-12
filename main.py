9e0522fc6d822a558079f598fc205706238249df
iamapez <acperez@syr.edu>
2022-04-11 22:18:17 -0400

    first commit in this branch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# sample entry point for program
 
 
 def print_hi(name):
@@ -9,8 +6,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def main():
+    print('hit main method')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    main()
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
