f822a41f5d4cb956ad5f40917324be6130958dfe
iamapez <acperez@syr.edu>
2022-04-12 12:55:38 -0400

    test

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,4 @@
 import streamlit as st
 
 
-st.markdown("### 🎲 The Application")
-st.markdown("This application is a Streamlit dashboard hosted on Heroku that can be used"
-            "to explore the results from board game matches that I tracked over the last year.")
-st.markdown("**♟ General Statistics ♟**")
-st.markdown("* This gives a general overview of the data including"
-            "frequency of games over time, most games played in a day, and longest break"
-            "between games.")
+st.markdown("The Application")
