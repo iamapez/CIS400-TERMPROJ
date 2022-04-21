aa4ea59ed6a6117db0b7370fd68fa6e0548c678d
iamapez <acperez@syr.edu>
2022-04-21 12:47:15 -0400

    some logic to find a person twitter username (timeout error)

--- backend/main.py
+++ backend/main.py
@@ -14,13 +14,14 @@ import parseCSV
 import twitterapi
 import json
 from Canidate import setFromRow
+from googlesearch import search
+
 
 def updateJSONData():
     """
     Method to write to JSON file based on csv pulled from internet
     :return: true if successful, false if failure
     """
-
 
     response = parseCSV.getInfoandUpdateJSON()
     if len(response) == 0:
@@ -37,7 +38,6 @@ def getUserObjectsFromJSON():
     :return: listOfObjects
     """
 
-
     pathToDataJSON = 'assets/CLEANconvertcsv.json'
     d = os.getcwd()  # change directories to access the json file containing the data
     os.chdir("..")
@@ -49,12 +49,17 @@ def getUserObjectsFromJSON():
     data = json.load(f)
     for i in data:
         # print(i)
-        tempCanidate = setFromRow(dateOfElection=i['election_dt'], county=i['county_name'], contestName=i['contest_name'],
+        tempCanidate = setFromRow(dateOfElection=i['election_dt'], county=i['county_name'],
+                                  contestName=i['contest_name'],
                                   nameOnBallot=i['name_on_ballot'], firstName=i['first_name'], lastName=i['last_name'],
-                                  nickName=i['nick_name'], streetAddress=i['street_address'], city=i['city'], state=i['state'],
-                                  zipCode=i['zip_code'], businessPhone=i['business_phone'], email=i['email'], candidacyDT=i['candidacy_dt'],
-                                  partyContest=i['party_contest'], partyCandidate=i['party_candidate'], isUnexpired=i['is_unexpired'],
-                                  hasPrimary=i['has_primary'], isPartisan=i['is_partisan'], voteFor=i['vote_for'], term=i['term'])
+                                  nickName=i['nick_name'], streetAddress=i['street_address'], city=i['city'],
+                                  state=i['state'],
+                                  zipCode=i['zip_code'], businessPhone=i['business_phone'], email=i['email'],
+                                  candidacyDT=i['candidacy_dt'],
+                                  partyContest=i['party_contest'], partyCandidate=i['party_candidate'],
+                                  isUnexpired=i['is_unexpired'],
+                                  hasPrimary=i['has_primary'], isPartisan=i['is_partisan'], voteFor=i['vote_for'],
+                                  term=i['term'])
         listOfObjects.append(tempCanidate)
 
     # Closing file
@@ -63,17 +68,30 @@ def getUserObjectsFromJSON():
     return listOfObjects
 
 
+def findTwitterUsername(nameOnBallot, state):
+    query = '{} {} twitter'.format(nameOnBallot, state)
+    for i in search(query, num_results=5):
+        if 'twitter.com' in i:
+            return i
+
+
 def main():
-    # electionObjects = updateJSONData()
+    """
+    Entry point for our backend code.
+    :return: exit code status
+    """
 
-    apez_Authenticated = twitterapi.getAuthenticated()
-    userObjects = getUserObjectsFromJSON()
+    # electionObjects = updateJSONData()            # only run this is we want to re initalize our data from the csv data file
 
-    for user in userObjects:
-        print(user.nameOnBallot)
+    apez_Authenticated = twitterapi.getAuthenticated()  # authenticate apez api key and store it here
+    userObjects = getUserObjectsFromJSON()  # get a list of user objects that we can do work with internally
+
+    for i in userObjects:
+        print(findTwitterUsername(i.nameOnBallot, i.state))
+
+    # populate the twitterusername attribute for each python object so we can use the api later
 
     exit(1)
-
 
 
 if __name__ == "__main__":
