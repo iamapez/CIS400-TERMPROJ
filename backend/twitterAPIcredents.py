b59b4301fd1f4179e47e59ceeb360bb0db40a1fc
Isabel Melo <immelo@syr.edu>
2022-04-26 14:13:11 -0400

    added my api credentials
    liz twitter api keys

--- backend/twitterAPIcredents.py
+++ backend/twitterAPIcredents.py
@@ -6,3 +6,8 @@ apez_consumerSecret = 'KNfawfPtKK1EjM051QK0sDyZXkyy8AioyOizk8I91e9dGAhEZB'
 apez_oauthtoken = '1491779748309213187-qQDrstYkxR0erUSHulk1XEN0inLJJU'
 apez_oauthsecret = 'eGu6osBEBd9NcRXX6I3VGC1Cw1UtiPQvngSij1FRwFoJ3'
 
+#Liz's twitter api credentials
+Liz_CONSUMER_KEY = 'D0T9wVVnYYAPvXs0iNBlUskoj'
+Liz_CONSUMER_SECRET = 'BgZ1eKYH1m2NxQhMWHDXax6RxeKhZsTRkCTuqN5gnXIpq8EWlg'
+Liz_OAUTH_TOKEN = '1493664080380448771-MMxe9GbsYuAOjq2x01Vdo6IozmZipS'
+Liz-OAUTH_TOKEN_SECRET = 'rt278bWsYfMATV83b6boWCAkrITaLst7JHtcVvPpr6L2R'
